8a3a4cb7c4641b14a1d2ba4518917a0929576e21
biboSguy <132067957+biboSguy@users.noreply.github.com>
2024-02-23 10:43:35 -0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,16 +1,26 @@
-def vm(distancia, tempo):
-    if tempo != 0:
-        return distancia / tempo
-    else:
-        return "Erro"
+def calculadora():
+    print("Calculadora legal de metros por segundo 2023 atualizada")
+    while True:
+        try:
+            def vm(distancia, tempo):
+                if tempo != 0:
+                    return distancia / tempo
+                else:
+                    return "Erro"
 
-print("Velocidade Média")
-distancia = float(input('''Qual a distância?
+            print("Velocidade Média")
+            distancia = float(input('''
+Qual a distância?
 (em metros): '''))
-print("Distância:", distancia)
-tempo = float(input('''Qual é o tempo? 
+            print("Distância:", distancia)
+            tempo = float(input('''
+Qual é o tempo? 
 (em segundos): '''))
-print("Tempo:", tempo)
+            print("Tempo:", tempo)
 
-resultado = vm(distancia, tempo)
-print("A velocidade média é:", resultado)
+            resultado = vm(distancia, tempo)
+            print("A velocidade média é:", resultado)
+
+        except ValueError:
+            print("Por favor, digite números válidos.")
+calculadora()
